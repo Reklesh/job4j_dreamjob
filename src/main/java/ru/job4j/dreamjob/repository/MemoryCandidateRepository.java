1b84e692c339e8eded37bb884635ecc480fa7805
Acer <a-krickiy@mail.ru>
2025-02-16 22:39:12 +0300

    7. Создание, просмотр, редактирование и удаление кандидатов [#504858]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -33,8 +33,8 @@ public class MemoryCandidateRepository implements CandidateRepository {
     }
 
     @Override
-    public void deleteById(int id) {
-        candidates.remove(id);
+    public boolean deleteById(int id) {
+        return Objects.nonNull(candidates.remove(id));
     }
 
     @Override
