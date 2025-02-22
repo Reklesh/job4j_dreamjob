bbfc415ada6d6d1a5d091a2d3d492daad31aec90
Acer <a-krickiy@mail.ru>
2025-02-23 00:52:02 +0300

    4. Многопользовательские приложения. Многопоточность в Веб приложении. [#504857]

--- src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryCandidateRepository.java
@@ -1,14 +1,22 @@
 package ru.job4j.dreamjob.repository;
 
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Candidate;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
+@ThreadSafe
 @Repository
 public class MemoryCandidateRepository implements CandidateRepository {
-    private int nextId = 1;
-    private final Map<Integer, Candidate> candidates = new HashMap<>();
+
+    private final AtomicInteger nextId = new AtomicInteger();
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private MemoryCandidateRepository() {
         save(new Candidate(0, "Ivan", "Intern Java Developer"));
@@ -21,9 +29,10 @@ public class MemoryCandidateRepository implements CandidateRepository {
 
     @Override
     public Candidate save(Candidate candidate) {
-        candidate.setId(nextId++);
-        candidates.put(candidate.getId(), candidate);
-        return candidate;
+        return candidates.computeIfAbsent(nextId.incrementAndGet(), k -> {
+            candidate.setId(k);
+            return candidate;
+        });
     }
 
     @Override
--- src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -1,14 +1,18 @@
 package ru.job4j.dreamjob.repository;
 
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Vacancy;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
+@ThreadSafe
 @Repository
 public class MemoryVacancyRepository implements VacancyRepository {
-    private int nextId = 1;
-    private final Map<Integer, Vacancy> vacancies = new HashMap<>();
+    private final AtomicInteger nextId = new AtomicInteger();
+    private final Map<Integer, Vacancy> vacancies = new ConcurrentHashMap<>();
 
     private MemoryVacancyRepository() {
         save(new Vacancy(0, "Intern Java Developer", "Разработка ПО"));
@@ -21,9 +25,10 @@ public class MemoryVacancyRepository implements VacancyRepository {
 
     @Override
     public Vacancy save(Vacancy vacancy) {
-        vacancy.setId(nextId++);
-        vacancies.put(vacancy.getId(), vacancy);
-        return vacancy;
+        return vacancies.computeIfAbsent(nextId.incrementAndGet(), k -> {
+            vacancy.setId(k);
+            return vacancy;
+        });
     }
 
     @Override
